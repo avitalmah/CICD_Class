f64a269aec2b263546510bf14b3a4b25fd42dba3
Ron Mansharof <mansharofron@gmail.com>
2022-03-14 12:05:00 +0200

    connect db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+
+
+//const dbUrl = mongodb+srv+"://theperfectgroup8:project2022@sceproject.aalci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = 'mongodb+srv://theperfectgroup8:project2022@sceproject.aalci.mongodb.net/check?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const Cat = mongoose.model('Cat', { name: String });
+
+//   const kitty = new Cat({ name: 'Zildjian' });
+//   kitty.save().then(() => console.log('meow'));
+//   client.close();
+// });
+
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('<h1>Hellow World </h1>');
 });
+
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+mongoose.connect(uri, connectionParams).then(() => {
+  console.log('MongoDB Connected');
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
+  console.log('Hii');
+}).catch(err => console.log(err));
+const Cat = mongoose.model('Cat', { name: String });
+
+
 
 app.listen(port, () => {
   console.log(`Server is up and running on http://127.0.0.1:${port}`);
