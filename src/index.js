435f2b8388d427eaaae5f7e31b43ed600d4fd0cb
Ron Mansharof <mansharofron@gmail.com>
2022-03-19 23:51:22 +0200

    fix linter errror

--- src/index.js
+++ src/index.js
@@ -30,18 +30,18 @@ const connectionParams = {
 
 mongoose.connect(dbUrl, connectionParams).then(() => {
   console.log('MongoDB Connected');
-  const kitty = new Cat({name: 'dani'});
+  const kitty = new Cat({ name: 'dani' });
   kitty.save().then(() => console.log('meow'));
   console.log('Hii');
 }).catch((err) => console.log(err));
-const Cat = mongoose.model('Cat', {name: String});
+const Cat = mongoose.model('Cat', { name: String });
 
 app.post('/Register', (req, res) => {
   console.log(req.body);
-  User.findOne({email: req.body.email}, (err, user) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
     if (user) {
       console.log('emaiil is already exist');
-      return res.status(401).json({email: 'Email already exists'});
+      return res.status(401).json({ email: 'Email already exists' });
     } else {
       const user = new User({
         firstName: req.body.first_name,
@@ -59,25 +59,25 @@ app.post('/Register', (req, res) => {
         if (err) {
           res.send(err);
         } else {
-          res.send({message: 'sucessfull'});
+          res.send({ message: 'sucessfull' });
         }
       });
     }
   });
 });
 
-app.post("/Login",(req,res)=>{
-  const {email,password} =req.body;
-  User.findOne({email:email},(err,user)=>{
-      if(user){
-         if(password === user.password){
-             res.send({message:"login sucess",user:user})
-         }else{
-             res.send({message:"wrong credentials"})
-         }
-      }else{
-          res.send("not register")
+app.post('/Login', (req, res) => {
+  const {email, password} = req.body;
+  User.findOne({email: email}, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({message: 'login sucess', user: user});
+      } else {
+        res.send({message: 'wrong credentials'});
       }
+    } else {
+      res.send('not register');
+    }
   })
 });
 
