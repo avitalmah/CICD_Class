23a54950eb8d563167f2ba2e559d5fd69f606b88
Ron Mansharof <mansharofron@gmail.com>
2022-03-21 13:22:17 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -84,10 +84,10 @@ app.post('/Login', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
